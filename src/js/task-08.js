7f52fc6cfbc757eff6bbdc8d2425e35f520f2e33
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2023-08-10 21:45:01 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,42 +1,18 @@
-// const loginForm = document.querySelector('.login-form');
+const loginForm = document.querySelector(".login-form");
 
-// loginForm.addEventListener('submit', handleSubmit);
-
-// function handleSubmit(event) {
-//   event.preventDefault(); // Забороняємо перезавантаження сторінки
-
-//   const formData = new FormData(event.currentTarget);
-//   const userData = {};
-
-//   formData.forEach((value, name) => {
-//     userData[name] = value;
-//   });
-
-//   if (!userData.email || !userData.password) {
-//     alert('All fields must be filled out');
-//     return;
-//   }
-
-//   console.log(userData);
-//   loginForm.reset();
-// }
-
-const loginForm = document.querySelector('.login-form');
-
-loginForm.addEventListener('submit', handleSubmit);
+loginForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault(); // Забороняємо перезавантаження сторінки
+  event.preventDefault();
 
   const formData = new FormData(event.currentTarget);
-  const { email, password } = Object.fromEntries(formData.entries()); // Деструктуризация объекта formData
-
+  const { email, password } = Object.fromEntries(formData.entries());
   if (!email || !password) {
-    alert('All fields must be filled out');
+    alert("All fields must be filled out");
     return;
   }
 
-  console.log('Email:', email);
-  console.log('Password:', password);
+  console.log("Email:", email);
+  console.log("Password:", password);
   loginForm.reset();
 }
